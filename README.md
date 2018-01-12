f69de861b67e7c03ed4abfaff92780e002b2697a
Tim Lochmüller <tim.lochmueller@hdnet.de>
2018-01-12 15:29:42 +0100

    Add TYPO3 8.x

--- README.md
+++
@@ -1,2 +0,0 @@
-# amt_feed_importer
-Fork of EXT:amt_feed_importer
--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.99.99',
+			'typo3' => '7.6.0-8.99.99',
 			'news' => '4.0.0-0.0.0'
 		),
 		'conflicts' => array(
